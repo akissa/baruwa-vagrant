a05c5587858a90df792be46dc7e2f337de39ec9e
Andrew Colin Kissa <andrew@topdog.za.net>
2016-01-12 10:43:45 +0200

    Make rsyslogd configuration idpotent

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,7 @@
     sed -i 's:distroverpkg=centos-release:distroverpkg=baruwa-release:' /etc/yum.conf
     sed -i 's|http://bugs.centos.org/set_project.php?project_id=19&ref=http://bugs.centos.org/bug_report_page.php?category=yum|http://www.baruwa.com|' /etc/yum.conf
 }
+/usr/bin/test -e /etc/rsyslog.d/ratelimit.conf || {
 cat << 'EOF' > /etc/rsyslog.d/ratelimit.conf
 $SystemLogRateLimitInterval 0
 $SystemLogRateLimitBurst 0
@@ -12,6 +13,7 @@ $IMUXSockRateLimitInterval 0
 $IMUxSockRateLimitSeverity 7
 EOF
 service rsyslog restart
+}
 [ `grep 'mirrors.kernel.org' /etc/yum.repos.d/CentOS-Base.repo` ] || {
     unalias cp || /bin/true
     cp -f /vagrant/scripts/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
