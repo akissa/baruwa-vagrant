4e02aca71e2bbadcc335b042454caa07f4755fdb
Andrew Colin Kissa <andrew@topdog.za.net>
2016-12-10 10:35:14 +0200

    Increase number of SSH requests

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,23 +4,13 @@
     sed -i 's:distroverpkg=centos-release:distroverpkg=baruwa-release:' /etc/yum.conf
     sed -i 's|http://bugs.centos.org/set_project.php?project_id=19&ref=http://bugs.centos.org/bug_report_page.php?category=yum|http://www.baruwa.com|' /etc/yum.conf
 }
-/usr/bin/test -e /etc/rsyslog.d/ratelimit.conf || {
-cat << 'EOF' > /etc/rsyslog.d/ratelimit.conf
-$SystemLogRateLimitInterval 0
-$SystemLogRateLimitBurst 0
-$IMUxSockRateLimitBurst 0
-$IMUXSockRateLimitInterval 0
-$IMUxSockRateLimitSeverity 7
-EOF
-service rsyslog restart
-}
 if [ -e /etc/yum.repos.d/CentOS-Base.repo ]; then
     [ `grep 'mirrors.kernel.org' /etc/yum.repos.d/CentOS-Base.repo` ] || {
         unalias cp 2>/dev/null || /bin/true
         cp -f /vagrant/scripts/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
     }
 fi
-if grep -Fxq '127.0.0.1 guest' /etc/hosts; then
+if egrep -xq '^# then you will need to either|127\.0\.0\.1 guest$' /etc/hosts; then
 cat << 'EOF' > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
@@ -66,13 +56,24 @@ if [ "$2" != "" ]; then
     hostname "$2"
     if [ "$?" = "0" ]; then
         export HOSTNAME="$2"
+        echo "$2" > /etc/hostname
     fi
 fi
 }
-if grep -Fxq 'HOSTNAME=vultr.guest' /etc/sysconfig/network; then
+if egrep -xq '^HOSTNAME=vultr.guest|HOSTNAME=digitalocean$' /etc/sysconfig/network; then
     sed -ie "s/^HOSTNAME=.*$/HOSTNAME=$(hostname)/" /etc/sysconfig/network
     if ! grep -Fxq 'PEERDNS=no' /etc/sysconfig/network-scripts/ifcfg-eth0; then
         echo 'PEERDNS=no' >> /etc/sysconfig/network-scripts/ifcfg-eth0
         echo 'make_resolv_conf(){:}' >> /etc/dhclient-enter-hooks
     fi
 fi
+/usr/bin/test -e /etc/rsyslog.d/ratelimit.conf || {
+cat << 'EOF' > /etc/rsyslog.d/ratelimit.conf
+$SystemLogRateLimitInterval 0
+$SystemLogRateLimitBurst 0
+$IMUxSockRateLimitBurst 0
+$IMUXSockRateLimitInterval 0
+$IMUxSockRateLimitSeverity 7
+EOF
+service rsyslog restart
+}
